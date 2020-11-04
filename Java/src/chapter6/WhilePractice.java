35780eb044e874f4f2f568612bbdf3c7f2691ef9
dongkey1198 <utdhkim94@gmail.com>
2020-11-04 17:46:40 +0900

    While

---
+++ Java/src/chapter6/WhilePractice.java
@@ -0,0 +1,25 @@
+package chapter6;
+
+import java.util.Scanner;
+
+public class WhilePractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		int num1;
+		
+		num1 = sc.nextInt();
+		
+		while(num1 != 0) {
+			System.out.print("정수를 입력하세요: ");
+			num1 = sc.nextInt();
+			sum += num1;
+		}
+		
+		System.out.println(sum);
+		
+	}
+
+}
---
+++ Java/src/chapter6/whileExample2.java
@@ -0,0 +1,22 @@
+package chapter6;
+
+public class whileExample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 1;
+		int sum = 0;
+		
+		do{
+			sum += num;
+			num++;//num은 11까지 올라간당
+		}while(num <= 10);
+		
+		
+		System.out.println(sum);
+		
+		
+	}
+
+}
