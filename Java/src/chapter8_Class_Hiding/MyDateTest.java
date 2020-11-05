3016cc454c04d7ef95b85e77d0554491c16ead73
dongkey1198 <utdhkim94@gmail.com>
2020-11-05 18:28:20 +0900

    Chapter8:Class - Hiding

---
+++ Java/src/chapter8_Class_Hiding/MyDateTest.java
@@ -0,0 +1,16 @@
+package chapter8_Class_Hiding;
+
+public class MyDateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyDate date = new MyDate();
+		
+		date.setYear(2019);
+		date.setMonth(10);
+		date.setDay(10);
+		
+		date.showDate();
+	}
+
+}
