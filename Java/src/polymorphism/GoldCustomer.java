8e47413d09df17f0f69c9a42493dce68e02f1a06
dongkey1198 <utdhkim94@gmail.com>
2020-11-12 16:09:51 +0900

    polymorphism

--- Java/src/polymorphism/GoldCustomer.java
+++ Java/src/polymorphism/GoldCustomer.java
@@ -1,5 +1,23 @@
 package polymorphism;
 
-public class GoldCustomer {
+public class GoldCustomer extends Customer {
+	
+	double salesRatio;
+	
+	public GoldCustomer(int customerID, String customerName) {
+		super(customerID, customerName);
+		
+		customerGrade ="GOLD";
+		bounusRatio = 0.02;
+		salesRatio = 0.1;
+	}
+
+	@Override
+	public int clacPrice(int price) {
+		bonusPoint += price * bounusRatio;
+		return price - (int)(price * salesRatio);
+	}
+	
+	
 
 }
