3d97045ad9721218060812b10bfc7ed3fca7a2e3
dongkey1198 <utdhkim94@gmail.com>
2020-11-17 13:19:27 +0900

    generic

---
+++ Java/src/generic/Powder.java
@@ -0,0 +1,12 @@
+package generic;
+
+public class Powder extends Meterial{
+	
+	public String toString() {
+		return "재료는 Powder입니다.";
+	}
+	
+	public void doPrinting() {
+		System.out.println("Powder로 프린팅 합니다.");
+	}
+}
