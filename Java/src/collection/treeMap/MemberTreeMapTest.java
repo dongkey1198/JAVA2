c6c0a406d80e27bb1b50d372719ac3a043bbe1be
dongkey1198 <utdhkim94@gmail.com>
2020-11-18 12:59:07 +0900

    HashMap/HashTree

---
+++ Java/src/collection/treeMap/MemberTreeMapTest.java
@@ -0,0 +1,24 @@
+package collection.treeMap;
+
+public class MemberTreeMapTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MemberTreeMap manager = new MemberTreeMap();
+		
+		Member memberPark = new Member(300, "Park");
+		Member memberLee = new Member(100, "Lee");
+		Member memberKim = new Member(200, "Kim");
+		Member memberPark2 = new Member(400, "Park2");
+		
+		manager.addMember(memberLee);
+		manager.addMember(memberKim);
+		manager.addMember(memberPark);
+		manager.addMember(memberPark2);
+		
+		manager.showAllMemeber();
+		manager.removeMember(200);
+		manager.showAllMemeber();
+	}
+
+}
