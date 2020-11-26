fb8098183cf1c35caf6be34be5e19d7fb5a03a10
dongkey1198 <utdhkim94@gmail.com>
2020-11-26 22:26:46 +0900

    Stream

--- Java/src/lambda/TestMyNumber.java
+++ Java/src/lambda/TestMyNumber.java
@@ -5,7 +5,7 @@ public class TestMyNumber {
 	public static void main(String[] args) {
 		
 		MyMaxNumber max = (x,y) -> (x>=y)?x: y;
-		System.out.println(max.getMaxNumber(10, 20));
+		System.out.println(max.getMaxNumber(10, 20 ));
 		
 	}
 
