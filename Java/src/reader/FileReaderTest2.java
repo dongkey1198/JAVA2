59cfaf6a20d7fa77e041ac13c958caedaae82e6a
dongkey1198 <utdhkim94@gmail.com>
2020-11-30 13:42:03 +0900

    Read/Write

---
+++ Java/src/reader/FileReaderTest2.java
@@ -0,0 +1,24 @@
+package reader;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FileReaderTest2 {
+
+	public static void main(String[] args) throws IOException {
+		
+		FileReader fis = new FileReader("reader.txt");
+		
+		int i;
+		while((i = fis.read()) != -1) {
+			System.out.print((char)i);
+		}
+		fis.close();
+	}
+
+
+
+}
